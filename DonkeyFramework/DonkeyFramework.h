ee3d221bd5dc93a766a0df41650d6677718508ff
Aleksander Maj <aleksander.maj@htdevelopers.com>
2018-09-12 01:25:55 +0200

    Set up CocoaPods

---
+++ DonkeyFramework/DonkeyFramework.h
@@ -0,0 +1,19 @@
+//
+//  DonkeyFramework.h
+//  DonkeyFramework
+//
+//  Created by Aleksander Maj on 12/09/2018.
+//  Copyright © 2018 HTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DonkeyFramework.
+FOUNDATION_EXPORT double DonkeyFrameworkVersionNumber;
+
+//! Project version string for DonkeyFramework.
+FOUNDATION_EXPORT const unsigned char DonkeyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DonkeyFramework/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+platform :ios, '10.0'
+
+target 'Donkey' do
+  use_frameworks!
+
+  target 'DonkeyTests' do
+    inherit! :search_paths
+  end
+
+end
+
+target 'DonkeyFramework' do
+  use_frameworks!
+
+  target 'DonkeyFrameworkTests' do
+    inherit! :search_paths
+  end
+
+end
